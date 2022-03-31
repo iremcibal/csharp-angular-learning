5cf29196a7db7f42b75383d285f9c76743d66fb5
İrem Balcı <info@irembalci.com>
2022-03-31 14:48:22 +0300

    Gün 4 Ödev 5

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey , Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+
+        public MyDictionary() { 
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+
+        } //Default Constructor
+        
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempArray1 = keys;
+            Tvalue[] tempArray2 = values;
+
+            keys = new Tkey[keys.Length+1];
+            values = new Tvalue[keys.Length+1];
+
+            for(int i = 0; i < tempArray1.Length; i++)
+            {
+                keys[i] = tempArray1[i];
+                values[i] = tempArray2[i];
+            }
+
+            keys[keys.Length-1] = key;
+            values[keys.Length-1] = value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        
+    }
+}
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int , string> myDictionary = new MyDictionary<int , string>();
+
+            myDictionary.Add(1, "iREM BALCI");
+            myDictionary.Add(2, "ENGİN DEMiROG");
+
+            Console.WriteLine(myDictionary.Count);
+            
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
